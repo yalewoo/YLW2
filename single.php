401e31a74bc7cd3951d76c8c6686a5aa42fe86d1
yalewoo <chongruyuntian@163.com>
2014-05-20 17:08:19 +0800

    增加章页面的来源显示

--- single.php
+++ single.php
@@ -10,10 +10,26 @@
                 <?php the_content(); ?>
                 <?php link_pages('<p><strong>Pages:</strong>', '</p>', 'number'); ?>
                 	<p class="postmetadata">
-                    分类： <?php the_category(', ') ?> by： <a href="http://weibo.com/u/1767420541?s=6uyXnP" target="_blank"><img border="0" src="http://service.t.sina.com.cn/widget/qmd/1767420541/8a47f048/1.png"/></a>
-                   </p>
+                    <strong> 分类: </strong> <?php the_category(', ') ?> 
+                    </p>   
+                    <?php   $custom_fields = get_post_custom_keys($post_id);
+    if (!in_array ('copyright', $custom_fields)) : ?>
+	<div class="postcopyright">
+	    <p><strong> 声明: </strong> 本文由(<a href="<?php bloginfo('home'); ?>"> <?php the_author(); ?> </a>)原创，转载请保留本文链接: <a href="<?php the_permalink()?>" title=<?php the_title(); ?>><?php the_permalink()?></a></p>
+	</div>
+   <?php else: ?>
+	<?php  $custom = get_post_custom($post_id);
+           $custom_value = $custom['copyright']; ?>
+	<div class="postcopyright">
+	   <p><strong> 声明: </strong> 本文来源于 <a rel="nofollow" target="_blank" href="/go.php?url=<?php echo $custom_value[0] ?>"><?php echo $custom_value[0] ?></a> ，由(<a href="<?php bloginfo('home'); ?>"> <?php the_author(); ?> </a>) 整编。</p>
+	  <p><strong> 本文链接: </strong><a href="<?php the_permalink()?>" title=<?php the_title(); ?>><?php the_permalink()?></a> .</p>
+        </div>
+	    <?php endif; ?>                               
                 
             </div>
+            
+            
+	    
             <div class="comments-template">
                 <?php comments_template('', true); ?>
             </div>
